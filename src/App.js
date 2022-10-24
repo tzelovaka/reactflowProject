f050d10e8cf6fad7ae5c89bec6b5f1d16be6aee0
Daria <Tr3deprinter@yandex.ru>
2022-10-24 23:51:25 +0300

    serverfirst

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
+import React, {useState, useEffect} from "react";
 import logo from './logo.svg';
 import './App.css';
-
+const tg = window.Telegram.WebApp.initData
 function App() {
+  const [data, setData] = useState(null)
+  useEffect(() => {
+    fetch ('https://enigmatic-citadel-15632.herokuapp.com/api')
+    .then((response) => response.json())
+    .then (response => setData(response.message))
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p><i>
+        {
+          !data ? "Загрузка..." : data
+        }  
+        </i></p>
+        
       </header>
     </div>
   );
