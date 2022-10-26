84bed0926e7d55570690b62b0bb2012bedcd5358
Daria <Tr3deprinter@yandex.ru>
2022-10-26 18:15:54 +0300

    serverfirst

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   const [data, setData] = useState(null)
   useEffect(() => {
-    fetch ('https://storinter.herokuapp.com//api')
+    fetch ('https://storinter.herokuapp.com/api')
     .then((response) => response.json())
     .then (response => setData(response.message))
   }, [])
