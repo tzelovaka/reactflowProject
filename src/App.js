6c07eb502629fa0c9c452ebd01a57cb9512595ac
Daria <Tr3deprinter@yandex.ru>
2022-10-28 18:49:51 +0300

    serverfirst

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     fetch ('https://storinter.herokuapp.com/api', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ message: `${tgid}`})
+      body: JSON.stringify({ id: `${tgid}`})
     })
     .then((response) => response.json())
     .then (response => setData(response.message))
