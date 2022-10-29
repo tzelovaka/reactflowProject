2e641a2cccd4e229424d8ef41209f73dfdf02a7a
Daria <Tr3deprinter@yandex.ru>
2022-10-29 19:41:34 +0300

    serverfirst

--- index.js
+++ index.js
@@ -45,6 +45,7 @@ app.get('/api', async (request, response) => {
     for (let i=0; i <= x; i++){
         blocks[i] = textbl[i].bl
     }
+    console.log(blocks);
     response.status(200) //устанавливает код ответа 200, ответ не отправлен
     return response.send({ message: blocks})
     }
