7e8def97a38de8b94cac300ca60b312fc72ce48c
Daria <Tr3deprinter@yandex.ru>
2022-10-29 15:39:08 +0300

    serverfirst

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get('/api', async (request, response) => {
         authId: `${data}`,
         release: false
     }});
-    const bl = await storybl.findAll({where:{
+    const bl = await storybl.findOne({where:{
         authId: `${data}`,
         release: false,
     }});
@@ -41,7 +41,7 @@ app.get('/api', async (request, response) => {
     }else{
     console.log(`${st.name}`);
     response.status(200) //устанавливает код ответа 200, ответ не отправлен
-    return response.send({ message: bl.bl })
+    return response.send({ message: `${bl.bl}` })
     }
 });
 /*app.post('/api', (req, res) => {
