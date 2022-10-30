06ddfd5466da3d1798cf589de33fb02911e2bc6c
Daria <Tr3deprinter@yandex.ru>
2022-10-30 16:25:01 +0300

    serverfirst

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
           }
           <br/>
         {
-          !data ? "Загрузка..." : data[0][2]
+          !data ? "Загрузка..." : data([0][2])
         }  
         </p>
       </header>
