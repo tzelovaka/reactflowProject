1867348ccc61abc1672e43460b0694aafbf97565
Daria <Tr3deprinter@yandex.ru>
2023-10-11 20:21:59 +0300

    make it better

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-const i = false;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {i && <App />}
+    <App />
   </React.StrictMode>
 );
 
