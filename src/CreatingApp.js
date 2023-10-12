160370a92f868b62c7eee41e2f84496e3e95005b
Daria <Tr3deprinter@yandex.ru>
2023-10-12 19:00:19 +0300

    make it better

--- src/CreatingApp.js
+++ src/CreatingApp.js
@@ -1,14 +1,13 @@
 import React, { useCallback, useState } from 'react';
-import 'reactflow/dist/style.css';
 import './index.css';
 
 const CreatingApp = () => {
-  const [title, setTitle] = useState();
+  const [title, setTitle] = useState('');
   const [imgUrl, setImgUrl] = useState('');
-  const [desc, setDesc] = useState('');
+  const [desc, setDesc] = useState();
   
   const onChange = useCallback((evt) => {
-    setTitle(evt.target.value);
+    setDesc(evt.target.value);
   }, []);
 
   const beginStory = useCallback((evt) => {
@@ -31,7 +30,7 @@ const CreatingApp = () => {
   }, [title, imgUrl, desc])
   return (
     <div>
-        <textarea className="w-full border-2 rounded-xl bg-slate-300 px-2 py-1 text-lg mx-2 my-3" rows={3} cols={30} value={desc} onChange={onChange}/> 
+        <textarea className="w-full border-2 rounded-xl bg-slate-300 px-2 py-1 text-lg mx-2 my-3" rows={3} cols={30} onChange={onChange}/> 
       
       <button className='my-5 mx-5 rounded-full bg-cyan-500 text-white text-lg px-5 py-2' type="submit" onClick={beginStory}>Создать</button>
       
