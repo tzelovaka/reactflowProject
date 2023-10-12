b1b8ae09f3d9b59062dc17e838b68170e3c9e3ee
Daria <Tr3deprinter@yandex.ru>
2023-10-12 11:54:08 +0300

    make it better

--- src/CreatingApp.js
+++ src/CreatingApp.js
@@ -19,12 +19,15 @@ const CreatingApp = ({ onSubmit }) => {
   return (
     <div>
       <form  onSubmit={handleSubmit} className='flex flex-wrap w-full h-full'>
-       <label id="title" className='mx-5 my-3'>Название истории</label>
+       <label><p id="title" className='mx-5 my-3'>Название</p>
         <input className='w-full mx-2 my-3 border-2 rounded-xl bg-slate-300' type="text" name="title" onChange={(e) => setTitle(e.target.value)}/>
-      <label id="imgurl" className='mx-5 my-3'>URL Обложки</label>
+      </label>
+      <label><p id="imgurl" className='mx-5 my-3'>URL Обложки</p>
         <input className='w-full mx-2 my-3 border-2 rounded-xl bg-slate-300' type="text" name="imgurl" onChange={(e) => setImgUrl(e.target.value)}/>
-      <label id="description" className='mx-5 my-3'>Описание</label>
+      </label>
+      <label><p id="description" className='mx-5 my-3'>Описание</p>
         <textarea className="w-full border-2 rounded-xl bg-slate-300 px-2 py-1 text-lg mx-2 my-3" name="desc" rows={3} cols={30} onChange={(e) => setDesc(e.target.value)}/> 
+      </label>
       <button className='my-5 mx-5 rounded-full bg-cyan-500 text-white text-lg px-5 py-2' type="submit">Создать</button>
       </form>
       
