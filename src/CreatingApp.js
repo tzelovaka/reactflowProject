f383777636b428fbe24c4488e4931986dee48929
Daria <Tr3deprinter@yandex.ru>
2023-10-12 11:06:49 +0300

    make it better

--- src/CreatingApp.js
+++ src/CreatingApp.js
@@ -14,11 +14,11 @@ useEffect(() => {
 }, [tgid])*/
   return (
     <div className='flex flex-wrap w-full h-full'>
-      <label>Название истории</label>
+      <label id="title" className='mx-5 my-3'>Название истории</label>
         <input className='w-full mx-5 my-3 border-2 rounded-xl bg-slate-300' type="name" name="" value=""/>
-      <label>URL Обложки</label>
+      <label id="imgurl" className='mx-5 my-3'>URL Обложки</label>
         <input className='w-full mx-5 my-3 border-2 rounded-xl bg-slate-300' type="name" name="" value=""/>
-      <label>Описание</label>
+      <label id="description" className='mx-5 my-3'>Описание</label>
         <textarea className="w-full border-2 rounded-xl bg-slate-300 px-2 py-1 text-lg mx-5 my-3" rows={3} cols={30}/>
       <input className='justify-self-end my-5 mx-5 rounded-full bg-cyan-500 text-white text-lg px-3 py-2' type="button"value="Создать"/>
     </div>
