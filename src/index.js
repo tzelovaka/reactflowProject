c29c919e603aa435686f2c7a58ff1ac9bc4f2c88
Daria <Tr3deprinter@yandex.ru>
2023-10-12 13:44:14 +0300

    make it better

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const i = false;
 const handleFormSubmit = (value) => {
-  console.log(value);
+  console.log(111111111111111111);
 }
 root.render(
   <React.StrictMode>
