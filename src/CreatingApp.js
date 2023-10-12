a93e8af884ad2f40586aaa386a8368fbc44ae507
Daria <Tr3deprinter@yandex.ru>
2023-10-12 13:48:48 +0300

    make it better

--- src/CreatingApp.js
+++ src/CreatingApp.js
@@ -3,31 +3,35 @@ import 'reactflow/dist/style.css';
 import './index.css';
 
 const CreatingApp = ({ onSubmit }) => {
-  //const [title, setTitle] = useState('');
-  //const [imgUrl, setImgUrl] = useState('');
-  //const [desc, setDesc] = useState('');
-
+  const [title, setTitle] = useState('');
+  const [imgUrl, setImgUrl] = useState('');
+  const [desc, setDesc] = useState('');
   const handleSubmit = (event) => {
-    console.log(event.target.value);
-    //event.preventDefault();
-    /*const data = {
+    event.preventDefault();
+    const data = {
       title: title,
       imgUrl: imgUrl,
       desc: desc
-    };*/
-    onSubmit(event.target.value);
+    };
+    console.log(data);
+    onSubmit(data);
   }
   return (
     <div>
       <form  onSubmit={handleSubmit} className='flex flex-wrap w-full h-full'>
        <label><p id="title" className='mx-5 my-3'>Название</p>
-        <input className='w-full mx-2 my-3 border-2 rounded-xl bg-slate-300' type="text" name="title" />
+        <input className='w-full mx-2 my-3 border-2 rounded-xl bg-slate-300' type="text" name="title" 
+        onChange={(e) => {
+          setTitle(e.target.value); 
+          console.log(title);
+          }
+          }/>
       </label>
       <label><p id="imgurl" className='mx-5 my-3'>URL Обложки</p>
-        <input className='w-full mx-2 my-3 border-2 rounded-xl bg-slate-300' type="text" name="imgurl"/>
+        <input className='w-full mx-2 my-3 border-2 rounded-xl bg-slate-300' type="text" name="imgurl" onChange={(e) => setImgUrl(e.target.value)}/>
       </label>
       <label><p id="description" className='mx-5 my-3'>Описание</p>
-        <textarea className="w-full border-2 rounded-xl bg-slate-300 px-2 py-1 text-lg mx-2 my-3" name="desc" rows={3} cols={30}/> 
+        <textarea className="w-full border-2 rounded-xl bg-slate-300 px-2 py-1 text-lg mx-2 my-3" name="desc" rows={3} cols={30} onChange={(e) => setDesc(e.target.value)}/> 
       </label>
       <button className='my-5 mx-5 rounded-full bg-cyan-500 text-white text-lg px-5 py-2' type="submit">Создать</button>
       </form>
