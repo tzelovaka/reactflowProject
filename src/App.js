18a63bb025b46163e7243310c6b23435edd4f264
Daria <Tr3deprinter@yandex.ru>
2023-10-12 16:38:42 +0300

    make it better

--- src/App.js
+++ src/App.js
@@ -72,35 +72,26 @@ useEffect(() => {
     },
     [project]
   );
-    //const [title, setTitle] = useState();
-    //const [imgUrl, setImgUrl] = useState();
-    //const [desc, setDesc] = useState();
-    const onChangeTitle = useCallback((evt) => {
-      console.log(evt.target.value);
-    }, [])
-    const onChangeImg = useCallback((evt) => {
-      console.log(evt.target.value);
-    }, [])
-    const onChangeDesc = useCallback((evt) => {
-      console.log(evt.target.value);
-    }, [])
-    const beginStory = useCallback((evt) => {
-      console.log(11111111111);
-    }, [])
+    const [title, setTitle] = useState();
+    const [imgUrl, setImgUrl] = useState();
+    const [desc, setDesc] = useState();
+    useEffect(() => {
+      console.log(title);
+}, [title])
   return (
     <div className="wrapper" style={{height: 800}} ref={reactFlowWrapper}>
       {!scheme && <div>
       <form className='flex flex-wrap w-full h-full'>
        <label><p id="title" className='mx-5 my-3'>Название</p>
-        <input className='w-full mx-2 my-3 border-2 rounded-xl bg-slate-300' type="text" name="title" onChange={onChangeTitle}/>
+        <input className='w-full mx-2 my-3 border-2 rounded-xl bg-slate-300' type="text" name="title" value={title}/>
       </label>
       <label><p id="imgurl" className='mx-5 my-3'>URL Обложки</p>
-        <input className='w-full mx-2 my-3 border-2 rounded-xl bg-slate-300' type="text" name="imgurl" onChange={onChangeImg}/>
+        <input className='w-full mx-2 my-3 border-2 rounded-xl bg-slate-300' type="text" name="imgurl"/>
       </label>
       <label><p id="description" className='mx-5 my-3'>Описание</p>
-        <textarea className="w-full border-2 rounded-xl bg-slate-300 px-2 py-1 text-lg mx-2 my-3" name="desc" rows={3} cols={30} onChange={onChangeDesc}/> 
+        <textarea className="w-full border-2 rounded-xl bg-slate-300 px-2 py-1 text-lg mx-2 my-3" name="desc" rows={3} cols={30}/> 
       </label>
-      <button className='my-5 mx-5 rounded-full bg-cyan-500 text-white text-lg px-5 py-2' type="submit" onClick={beginStory}>Создать</button>
+      <button className='my-5 mx-5 rounded-full bg-cyan-500 text-white text-lg px-5 py-2' type="submit">Создать</button>
       </form>
       
     </div>}
