5ae3b6d993807061f005621ee9f1dbb63b08c852
Daria <Tr3deprinter@yandex.ru>
2023-10-13 12:18:52 +0300

    make it better

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ const fitViewOptions = {
 
 
 const AddNodeOnEdgeDrop = () => {
-const [text, setText] = useState("Privet")
+const [text, setText] = useState()
 const [scheme, setScheme] = useState()
 const tgid = window.Telegram.WebApp.initDataUnsafe.user.id;
 useEffect(() => {
