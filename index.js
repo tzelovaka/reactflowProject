cde9d0c773b0be02703f8c2e7cd64254ec0bf29e
Daria <Tr3deprinter@yandex.ru>
2023-10-14 15:57:53 +0300

    make it better

--- index.js
+++ index.js
@@ -106,8 +106,7 @@ app.get('/api', async (request, response) => {
         authId = request.query.authId;
         storyId = request.query.storyId;
         const bl = await storybl.create({title: 'Блок', storyId: storyId, authId: authId})
-        console.log(bl);
-        return response.send({ message: bl.id })
+        return response.send({ message: bl.dataValues.id })
 }
 });
 
