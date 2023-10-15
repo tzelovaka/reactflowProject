db39dfa2de10f06e2804467e3d98349e2d89f904
Daria <Tr3deprinter@yandex.ru>
2023-10-15 19:03:42 +0300

    make it better

--- src/App.js
+++ src/App.js
@@ -121,27 +121,27 @@ const fitViewOptions = {
   return (
     <div className="wrapper" style={{height: screenHeight}} ref={reactFlowWrapper}>
       {cover && 
-      <div className='w-full grid grid-col-1'>
-          <div className='justify-self-end'>
-            <button className="rounded-xl px-4 h-8 my-2 bg-retro text-white mr-2 text-xl" onClick={e => setCover(false)}>–</button>
-          </div>
-          <div className='justify-self-center'>
-              <p id='label' className='text-lg mx-3 mt-4 font-philosopher'>Название</p>
-              <input type="text" className="mx-2 w-full font-philosopher border-2 rounded-xl bg-slate-300 px-5 py-1 text-lg mt-2 text-center" onChange={e => setTitle(e.target.value)} />
-            </div>
-          <div className='justify-self-center'>
-              <p id='label' className='text-lg mx-3 mt-4 font-philosopher'>URL картинки</p>
-            <input type="text" className=" w-full mx-2 font-philosopher border-2 rounded-xl bg-slate-300 px-5 py-1 text-lg mt-2" onChange={e => setImgUrl(e.target.value)} />
-            </div>
-          <div className='justify-self-center'>
-              <p id='label' className='text-lg mx-3 mt-4 font-philosopher'>Описание</p>
-            <textarea className="w-full mx-2 font-philosopher border-2 rounded-xl bg-slate-300 px-2 py-1 text-lg mt-2" rows={4} onChange={e => setDesc(e.target.value)} />
-            </div>
-          <div className='justify-self-center mt-4'>
-              <button className='bg-sea font-philosopher text-white font-bold py-2 px-4 rounded-full mx-3 text-md' onClick={onChange}>Сохранить</button>
-            <button className='bg-sea font-philosopher text-white font-bold py-2 px-4 rounded-full mx-3 text-md' onClick={onChange}>Опубликовать</button>
-            </div>
+      <div className='w-full grid grid-cols-1'>
+      <div className='justify-self-end'>
+        <button className="rounded-xl px-4 h-8 my-2 bg-retro text-white mr-2 text-xl" onClick={e => setCover(false)}>–</button>
       </div>
+      <div className='justify-self-center'>
+        <p id='label' className='text-lg mx-3 mt-4 font-philosopher'>Название</p>
+        <input type="text" className="w-full font-philosopher border-2 rounded-xl bg-slate-300 px-5 py-1 text-md mt-2 text-center" onChange={e => setTitle(e.target.value)} />
+      </div>
+      <div className='justify-self-center'>
+        <p id='label' className='text-lg mx-3 mt-4 font-philosopher'>URL картинки</p>
+        <input type="text" className="w-full font-philosopher border-2 rounded-xl bg-slate-300 px-5 py-1 text-md mt-2" onChange={e => setImgUrl(e.target.value)} />
+      </div>
+      <div className='justify-self-center'>
+        <p id='label' className='text-lg mx-3 mt-4 font-philosopher'>Описание</p>
+        <textarea className="w-full font-philosopher border-2 rounded-xl bg-slate-300 px-2 py-1 text-md mt-2" rows={4} onChange={e => setDesc(e.target.value)} />
+      </div>
+      <div className='justify-self-center mt-4'>
+        <button className='bg-sea font-philosopher text-white font-bold py-2 px-4 rounded-full mx-3 text-md' onClick={onChange}>Сохранить</button>
+        <button className='bg-sea font-philosopher text-white font-bold py-2 px-4 rounded-full mx-3 text-md' onClick={onChange}>Опубликовать</button>
+      </div>
+    </div>
       }
       {!cover && 
       <ReactFlow
