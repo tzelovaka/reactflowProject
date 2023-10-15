4b304158013ca3d6fb4287674c93f8887b3a1729
Daria <Tr3deprinter@yandex.ru>
2023-10-15 18:40:58 +0300

    make it better

--- src/App.js
+++ src/App.js
@@ -121,33 +121,25 @@ const fitViewOptions = {
   return (
     <div className="wrapper" style={{height: screenHeight}} ref={reactFlowWrapper}>
       {cover && 
-      <div className='w-full grid grid-cols-1 justify-items-center'>
+      <div className='w-full flex flex-col justify-items-center'>
           <div className='w-full'>
             <button className="rounded-xl px-4 h-8 my-2 bg-retro text-white mr-2 text-xl" onClick={e => setCover(false)}>–</button>
           </div>
           <div className='w-full justify-self-center'>
-            <div>
               <p id='label' className='text-lg mx-3 mt-4 font-philosopher'>Название</p>
             <input type="text" className="mx-2 w-10/12 font-philosopher border-2 rounded-xl bg-slate-300 px-5 py-1 text-lg mt-2 text-center" onChange={e => setTitle(e.target.value)} />
             </div>
-            </div>
           <div className='w-full'>
-            <div>
               <p id='label' className='text-lg mx-3 mt-4 font-philosopher'>URL картинки</p>
             <input type="text" className="mx-2 font-philosopher border-2 rounded-xl bg-slate-300 px-5 py-1 text-lg mt-2 w-10/12" onChange={e => setImgUrl(e.target.value)} />
             </div>
-            </div>
           <div className='w-full'>
-            <div>
               <p id='label' className='text-lg mx-3 mt-4 font-philosopher'>Описание</p>
             <textarea className="mx-2 font-philosopher border-2 rounded-xl bg-slate-300 px-2 py-1 text-lg mt-2 w-10/12" rows={3} cols={30} onChange={e => setDesc(e.target.value)} />
             </div>
-            </div>
           <div className='w-full'>
-            <div>
               <button className='bg-sea font-philosopher text-white font-bold py-2 px-4 rounded-full mx-3 text-md' onClick={onChange}>Сохранить</button>
             <button className='bg-sea font-philosopher text-white font-bold py-2 px-4 rounded-full mx-3 text-md' onClick={onChange}>Опубликовать</button>
-            </div>
             </div>
       </div>
       }
