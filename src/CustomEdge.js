b429b368dd28fb17ac0f4a6819321c81c622144e
Daria <Tr3deprinter@yandex.ru>
2023-10-16 19:45:54 +0300

    make it better

--- src/CustomEdge.js
+++ src/CustomEdge.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Handle, Position, useReactFlow, useNodesState, useNodes, useEdges } from 'reactflow';
+import { Handle, Position, useReactFlow, useEdges } from 'reactflow';
 
 import './index.css';
 
@@ -8,59 +8,55 @@ function CustomEdge({ id, sourceX, sourceY, targetX, targetY }) {
   const edges = useEdges();
   const handleInputChange = (event) => {
     setLabel(event.target.value);
-    let i;
-    edges.forEach((edge) => {if (edge.id === id) i=edges.indexOf(edge) });
-    edges[i].data.label = event.target.value;
-    console.log(edges);
+    const updatedEdges = edges.map((edge) => {
+      if (edge.id === id) {
+        return { ...edge, data: { ...edge.data, label: event.target.value } };
+      }
+      return edge;
+    });
+    console.log(updatedEdges);
   };
-  const curveX = targetX-125;
-  const curveY = targetY-100;
-  const [emj, setEmj] = useState('')
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const curveX = targetX - 125;
+  const curveY = targetY - 100;
+  const [emj, setEmj] = useState('');
+  const EmojiDropdown = () => {
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
     const emojis = [
       "😀", "😃", "😄", "😁", "😆", "😅", "😂", "🤣", "😊", "😇",
-  ];
-  const handleToggleDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen);
-  };
-  const emojiSelect = (emoji) => {
-    let i;
-    edges.forEach((edge) => {if (edge.id === id) i=edges.indexOf(edge) });
-    edges[i].data.smile = emoji;
-    setEmj(`${emoji}`)
-  };
-  return (
-    <>
-      <path
-        id={id}
-        //d={`M${sourceX} ${sourceY}, L${targetX} ${targetY}`}
-        // x={(sourceX + targetX) / 2} y={(sourceY + targetY) / 2}
-        d={`M${sourceX},${sourceY} C${sourceX},${curveY} ${targetX},${curveY} ${targetX},${targetY}`}
-        className="CustomEdge"
-      />
-      <foreignObject x={curveX+50} y={curveY+50} width="1280" height="2400">
-        <div className="flex items-center">
-          <input
-          type="text"
-          value={label}
-          onChange={handleInputChange}
-          className='border-2 border-cyan-600 rounded-full px-2 py-1 text-sm mr-8'
-        />
+      "😉", "😌", "😍", "😘", "😗", "😙", "😚", "😋", "😛", "😝",
+      "😜", "🤪", "🤨", "🧐", "🤓", "😎", "🤩", "🥳", "😏", "😒",
+    ];
 
-        <div className="relative inline-block">
+    const handleToggleDropdown = () => {
+      setIsDropdownOpen(!isDropdownOpen);
+    };
+
+    const emojiSelect = (emoji) => {
+      const updatedEdges = edges.map((edge) => {
+        if (edge.id === id) {
+          return { ...edge, data: { ...edge.data, smile: emoji } };
+        }
+        return edge;
+      });
+      console.log(updatedEdges);
+      setEmj(emoji);
+    };
+
+    return (
+      <div className="relative inline-block">
         <button
           onClick={handleToggleDropdown}
           className="px-4 py-2 text-sm font-medium text-white rounded-full bg-yellow-300 mr-2"
         >
-          {emj ? `${emj}` : `:)`}
+          {emj ? emj : ":)"}
         </button>
         {isDropdownOpen && (
           <div className="absolute mt-2 border-1 shadow-xl inline-block py-2 rounded-lg border-2 pl-2 pr-8">
-            <div className='grid grid-cols-12 gap-x-8 gap-y-2'>
+            <div className='grid grid-cols-12 gap-x-6 gap-y-2'>
               {emojis.map((emoji, index) => (
                 <div
                   key={index}
-                  className="text-3xl"
+                  className="cursor-pointer hover:bg-gray-100"
                   onClick={() => emojiSelect(emoji)}
                 >
                   {emoji}
@@ -70,8 +66,26 @@ function CustomEdge({ id, sourceX, sourceY, targetX, targetY }) {
           </div>
         )}
       </div>
+    );
+  };
+
+  return (
+    <>
+      <path
+        id={id}
+        d={`M${sourceX},${sourceY} C${sourceX},${curveY} ${targetX},${curveY} ${targetX},${targetY}`}
+        className="CustomEdge"
+      />
+      <foreignObject x={curveX + 50} y={curveY + 50} width="1280" height="2400">
+        <div className="flex items-center">
+          <input
+            type="text"
+            value={label}
+            onChange={handleInputChange}
+            className='border-2 border-cyan-600 rounded-full px-2 py-1 text-sm mr-8'
+          />
+          <EmojiDropdown />
         </div>
-        
       </foreignObject>
     </>
   );
