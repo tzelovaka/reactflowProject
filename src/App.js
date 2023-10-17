deecfe6660b3001369e858bf2054aa43266166f2
Daria <Tr3deprinter@yandex.ru>
2023-10-17 15:56:00 +0300

    make it better

--- src/App.js
+++ src/App.js
@@ -222,7 +222,7 @@ const fitViewOptions = {
           –
         </button>
       </div>
-        <div className='grid grid-cols-9 gap-x-8 gap-y-2 w-screen p-2'>
+        <div className='grid grid-cols-8 gap-x-8 gap-y-2 w-screen p-2'>
               {emojis.map((emoji, index) => (
                 <div
                   key={index}
