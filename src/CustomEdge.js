aea14e99df96ff241613e008856e31ec9a12efea
Daria <Tr3deprinter@yandex.ru>
2023-10-17 18:23:44 +0300

    make it better

--- src/CustomEdge.js
+++ src/CustomEdge.js
@@ -81,7 +81,7 @@ function CustomEdge({ id, sourceX, sourceY, targetX, targetY, props }) {
         d={`M${sourceX},${sourceY} C${sourceX},${curveY} ${targetX},${curveY} ${targetX},${targetY}`}
         className="CustomEdge"
       />
-      <foreignObject x={curveX+50} y={curveY + 35} width="250" height="250">
+      <foreignObject x={curveX} y={curveY + 40} width="500" height="250">
         <div className="flex flex-col w-full">
           <div className='flex'>
             <div className='grow h-14'>
