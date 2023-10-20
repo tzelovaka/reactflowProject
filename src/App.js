a8aa5bb1eab7807fe6cf396dfed5b5a358398db1
Daria <Tr3deprinter@yandex.ru>
2023-10-20 17:10:34 +0300

    make it better

--- src/App.js
+++ src/App.js
@@ -27,6 +27,21 @@ let id = 1;
 const getId = () => `${id++}`;
 
 const AddNodeOnEdgeDrop = () => {
+  const tgid = window.Telegram.WebApp.initDataUnsafe.user.id;
+useEffect(() => {
+  fetch(`https://storinter.herokuapp.com/api/?data=${tgid}`, {
+      method: 'GET',
+  })
+.then(response => response.json())
+.then (response => {
+setScheme(response.message)
+setNodes(response.message[1])
+if (response.message[2].length>0) setEdges(response.message[2])
+  })
+.catch(error => {
+console.error('Error:', error);
+});
+}, [tgid])
   const dispatch = useDispatch()
   const emojiWindowIsOpen = useSelector(state => state.window.emojiWindowIsOpen)
   const edgeId = useSelector(state => state.window.edgeId)
@@ -52,7 +67,7 @@ const [title, setTitle] = useState('');
 const [imgUrl, setImgUrl] = useState('');
 const [desc, setDesc] = useState('');
 const [scheme, setScheme] = useState()
-const tgid = window.Telegram.WebApp.initDataUnsafe.user.id;
+
 const controlsConfig = {
   showZoom: false,
   showFitView: false,
@@ -86,21 +101,6 @@ const animatedText = useSpring({
   to: {opacity: 1, transform: "translateY(0rem)"},
   reverse: !textAnimate
 });
-
-useEffect(() => {
-      fetch(`https://storinter.herokuapp.com/api/?data=${tgid}`, {
-          method: 'GET',
-      })
-  .then(response => response.json())
-  .then (response => {
-    setScheme(response.message)
-    setNodes(response.message[1])
-    if (response.message[2].length>0) setEdges(response.message[2])
-      })
-.catch(error => {
-  console.error('Error:', error);
-});
-}, [tgid])
 
 const fitViewOptions = {
   padding: 3,
