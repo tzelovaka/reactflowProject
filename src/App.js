727456872b0287a3207a39898f09df3a3229a540
Daria <Tr3deprinter@yandex.ru>
2023-10-21 23:39:06 +0300

    make it beter

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ const proOptions = { hideAttribution: true };
 let id = 1;
 const getId = () => `${id++}`;
 
-const AddNodeOnEdgeDrop = () => {
+const App= () => {
   const dispatch = useDispatch()
   const emojiWindowIsOpen = useSelector(state => state.window.emojiWindowIsOpen)
   const edgeId = useSelector(state => state.window.edgeId)
@@ -314,6 +314,6 @@ const fitViewOptions = {
 
 export default () => (
   <ReactFlowProvider>
-    <AddNodeOnEdgeDrop />
+    <App />
   </ReactFlowProvider>
 );
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import './index.css';
-import App from './App';
+import AddNodeOnEdgeDrop from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './store/index'
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-      <App />
+      <AddNodeOnEdgeDrop />
    
   </React.StrictMode>
 );
