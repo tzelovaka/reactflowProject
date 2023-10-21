1915d682bd8fc4da113ccf3bddfac04e71c18373
Daria <Tr3deprinter@yandex.ru>
2023-10-21 23:22:47 +0300

    make it beter

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ var path = require("path");
 
 app.use(express.json())
 //app.use(express.static(path.join(__dirname, '/build')));
-app.use(express.static(path.join(__dirname, 'build')));
+//app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-//app.use (express.static('build'));
+//app.get('/', function (req, res) {
+ // res.sendFile(path.join(__dirname, 'build', 'index.html'));
+//});
+app.use (express.static('build'));
 app.use(bodyParser.json());
 /*app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/build', 'index.html'));
