0cf28a1b7c2efde6944e96bc4d0a7b27cc2546af
Daria <Tr3deprinter@yandex.ru>
2023-10-21 13:19:39 +0300

    make it better

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 var path = require("path");
 
 app.use(express.json())
-app.use(express.static(path.join(__dirname, '/build')));
+//app.use(express.static(path.join(__dirname, '/build')));
 app.use (express.static('build'));
 app.use(bodyParser.json());
 /*app.get('*', (req, res) => {
