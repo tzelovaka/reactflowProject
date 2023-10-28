81137826baa6a0538197a43f6349144b2d193cbc
Daria <Tr3deprinter@yandex.ru>
2023-10-28 12:46:13 +0300

    make it better

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ app.post('/api/story', async (req, res) => {
         await story.create({ img: `${head.imgUrl}`, title: `${head.title}`, desc: `${head.desc}`, authId: `${head.authId}`});
     }
     const s = await story.findOne({where:{authId: `${head.authId}`, release: false}})
+    console.log(nodes);
     nodes.forEach(async (node) => {
         const imakillyou = await storybl.findOne({where :{
             fId: `${node.id}`,
