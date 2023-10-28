f72e31bd1f5b8705fb8363a3bbef1fd2d662eee7
Daria <Tr3deprinter@yandex.ru>
2023-10-29 01:00:31 +0300

    make it better

--- src/CustomEdge.js
+++ src/CustomEdge.js
@@ -11,9 +11,9 @@ function CustomEdge({ id, source, target, data}) { //sourceX, sourceY, targetX, 
   const nodes = getNodes()
   const sourceNode = nodes.find(node => node.id === source);
   const targetNode = nodes.find(node => node.id === target);
-  const sourceX = sourceNode.position.x+175;
-  const sourceY = sourceNode.position.y+150;
-  const targetX = targetNode.position.x+150;
+  const sourceX = sourceNode.position.x+10;
+  const sourceY = sourceNode.position.y+10;
+  const targetX = targetNode.position.x+10;
   const targetY = targetNode.position.y;
   const [label, setLabel] = useState(data.label);
   //const dispatch = useDispatch()
