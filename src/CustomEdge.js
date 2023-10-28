7434583c006f2e9b40df61f31db10fe0ee16f6ca
Daria <Tr3deprinter@yandex.ru>
2023-10-28 21:31:47 +0300

    make it better

--- src/CustomEdge.js
+++ src/CustomEdge.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import   useReactFlow, { useEdges } from 'reactflow';
+import { useReactFlow, useEdges } from 'reactflow';
 //import { useDispatch, useSelector } from 'react-redux';
 import './index.css';
 import { useEmoji } from './store/storeEmoji';
