67ebb0cd5f36247e76ad611295e83ad326527a57
Daria <Tr3deprinter@yandex.ru>
2023-10-29 12:00:01 +0300

    make it better

--- src/CustomEdge.js
+++ src/CustomEdge.js
@@ -1,18 +1,14 @@
-import React, { useEffect, useState,  } from 'react';
-import   { useReactFlow, useEdges, getNodes } from 'reactflow';
+import React, { useEffect, useState, useReactFlow } from 'react';
+import  { useEdges } from 'reactflow';
 //import { useDispatch, useSelector } from 'react-redux';
 import './index.css';
 import { useEmoji } from './store/storeEmoji';
 
-function CustomEdge({ id,  target, source, data}) {//sourceX, sourceY, targetX, targetY,
+function CustomEdge({ id, sourceX, sourceY, targetX, targetY, data}) {
   
   const [label, setLabel] = useState(data.label);
-  const { getNodes} = useReactFlow();
-  const nds = getNodes();
-  const sourceX = nds.find(nd => nd.id === source).data.customX
-  const sourceY = nds.find(nd => nd.id === source).data.customY
-  const targetX = nds.find(nd => nd.id === target).data.customX
-  const targetY = nds.find(nd => nd.id === target).data.customY
+  //const dispatch = useDispatch()
+  //const emojiWindowIsOpen = useSelector(state => state.emojiWindowIsOpen)
   const switchEmoji = useEmoji(state => state.switchEmoji)
   const handleInputChange = (event) => {
     if (event.target.value.length <= 280){
@@ -20,6 +16,12 @@ function CustomEdge({ id,  target, source, data}) {//sourceX, sourceY, targetX, 
     setLabel(newValue);
     data.label = newValue;
     }
+    /*const updatedEdges = edges.map((edge) => {
+      if (edge.id === id) {
+        return { ...edge, data: { ...edge.data, label: event.target.value } };
+      }
+      return edge;
+    });*/
   };
   const curveY = targetY - 100;
   const EmojiDropdown = () => {
