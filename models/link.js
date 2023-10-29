98f9e75f5ac107a88ed6b3884e6676442a5a5c9d
Daria <Tr3deprinter@yandex.ru>
2023-10-29 14:20:37 +0300

    make it better

--- models/link.js
+++ models/link.js
@@ -3,7 +3,7 @@ const {DataTypes} = require('sequelize')
 
 const storylin = sequelize.define ('storylin', {
     id: {type: DataTypes.INTEGER, primaryKey: true, unique: true, autoIncrement: true},
-    fId: {type: DataTypes.BIGINT},
+    fId: {type: DataTypes.TEXT},
     smile: {type: DataTypes.STRING(5), defaultValue: '👆'},
     text: {type: DataTypes.TEXT},
     storyId: {type: DataTypes.BIGINT, unique: false},
