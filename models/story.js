9168e6ce5426f07eb3c56334cc09500457e3c934
Daria <Tr3deprinter@yandex.ru>
2023-11-05 19:44:07 +0300

    make it better

--- models/story.js
+++ models/story.js
@@ -1,14 +1,18 @@
-const sequelize = require('../db')
-const {DataTypes} = require('sequelize')
+const sequelize = require('./db')
+const storylin = require('./modelink')
+const {DataTypes} = require('sequelize');
 
-const story = sequelize.define ('story', {
+const storybl = sequelize.define ('storybl', {
     id: {type: DataTypes.BIGINT, primaryKey: true, unique: true, autoIncrement: true},
-    views: {type: DataTypes.BIGINT, unique: false, defaultValue: 0},
+    fId: {type: DataTypes.BIGINT},
     img: {type: DataTypes.TEXT, allowNull: true, defaultValue: null},
-    title: {type: DataTypes.STRING},
-    desc: {type: DataTypes.STRING},
+    text: {type: DataTypes.TEXT},
+    positionX: {type: DataTypes.BIGINT},
+    positionY: {type: DataTypes.BIGINT},
+    storyId: {type: DataTypes.BIGINT, unique: false},
     authId: {type: DataTypes.BIGINT, unique: false},
     release: {type: DataTypes.BOOLEAN, defaultValue: false}
 })
+storybl.hasMany(storylin);
 
-module.exports = story;
+module.exports = storybl;
